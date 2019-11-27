44ae1e6fa3807ff590e59cfd881200638d138a6b
OutstandingOof <outstandingoof@gmail.com>
2019-11-27 17:56:15 +0100

    Changes to the default keymap

--- keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
@@ -1,12 +1,14 @@
-/* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
+/* A standard layout for the Ducktyl Manuform 5x6 Keyboard */ 
 
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _DVORAK 1
+#define _LOWER 2
+#define _RAISE 3
+#define _GAME 5
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
@@ -16,12 +18,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6(
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_LSFT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+                                      TG(_GAME),KC_SPC,                       KC_ENT, LOWER,
+                                      KC_LGUI,KC_ESC,                        KC_BSPC,KC_LGUI,
+                                      KC_BSPC, KC_GRV,                        KC_MPLY,KC_LALT
+  ), 
+
+  [_DVORAK] = LAYOUT_5x6(
+     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+     KC_TAB , KC_Q  , KC_COMM, KC_DOT, KC_P , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_MINS,
+     KC_LSFT, KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , KC_T  , KC_N  , KC_S  ,KC_LSFT,
+     KC_LCTL, KC_Z  , KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_BSLASH,
+                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
+                                      TG(_GAME),KC_SPC,                       KC_ENT, LOWER,
+                                      KC_LGUI,KC_ESC,                        KC_BSPC,KC_LGUI,
+                                      KC_BSPC, KC_GRV,                        KC_MPLY,KC_LALT
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -46,6 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
+  ), 
+
+  [_GAME] = LAYOUT_5x6(
+     _______, KC_5  , KC_1  , KC_2  , KC_3  , KC_4  ,                         _______,_______,_______,_______,_______,_______,
+     _______, KC_TAB, KC_Q  , KC_W  , KC_E  , KC_R  ,                         _______,_______,_______,_______,_______,_______,
+     _______, KC_LSFT,KC_A  , KC_S  , KC_D  , KC_F  ,                         _______,_______,_______,_______,_______,_______,
+     _______, KC_B  , KC_Z  , KC_X  , KC_C  , KC_V  ,                         _______,_______,_______,_______,_______,_______,
+                     _______,_______,                                                         _______,_______,
+                                      KC_LALT ,KC_SPC,                        _______,_______,
+                                      _______,  KC_G ,                        _______,_______,
+                                      _______, TG(_GAME),                     _______,_______
   ),
 };
 
