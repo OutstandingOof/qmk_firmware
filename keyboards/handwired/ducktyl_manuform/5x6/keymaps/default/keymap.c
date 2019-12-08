da39e442ac3a34984e4639c31f5636aae94effe8
OutstandingOof <outstandingoof@gmail.com>
2019-12-08 20:02:30 +0100

    All german keys added to the dvorak layout

--- keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
@@ -4,8 +4,8 @@
 
 extern keymap_config_t keymap_config;
 
-#define _QWERTY 0
-#define _DVORAK 1
+#define _QWERTY 1
+#define _DVORAK 0
 #define _LOWER 2
 #define _RAISE 3
 #define _GAME 5
@@ -23,25 +23,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                       RAISE ,KC_SPC ,                         KC_ENT, LOWER,
                                       KC_LGUI,KC_ESC,                         KC_BSPC,KC_LGUI,
-                                      KC_BSPC, KC_GRV,                        KC_MPLY,KC_LALT
+                                      KC_LALT,KC_GRV,                        KC_MPLY,KC_RALT
   ), 
 
   [_DVORAK] = LAYOUT_5x6(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_COMM, KC_DOT, KC_P , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_MINS,
+     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+     KC_TAB ,KC_SLSH, KC_COMM, KC_DOT, KC_P , KC_Z  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_BSLASH,
      KC_LSFT, KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , KC_T  , KC_N  , KC_S  ,KC_LSFT,
-     KC_LCTL, KC_Z  , KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE ,KC_SPC,                       KC_ENT, LOWER,
-                                      KC_LGUI,KC_ESC,                        KC_BSPC,KC_LGUI,
-                                      KC_BSPC, KC_GRV,                        KC_MPLY,KC_LALT
+     KC_LCTL,KC_NUBS, KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Y  ,KC_RCTL,
+                     KC_INS ,KC_CAPS,                                                        KC_RBRC,KC_EQL ,
+                                     RAISE  ,KC_SPC ,                        KC_ENT, LOWER,
+                                     KC_LGUI,KC_ESC ,                        KC_BSPC,KC_LGUI,
+                                     KC_LALT,KC_RALT,                        KC_MPLY,KC_DEL
   ),
 
   [_LOWER] = LAYOUT_5x6(
 
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+     _______,KC_QUOT,KC_SCLN,KC_PGDN,KC_LBRC,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
      _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                                              _______,KC_PSCR,            _______, KC_P0,
                                              _______,_______,            _______,_______,
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,TO(_GAME),KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        DF(_DVORAK),KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,DF(_QWERTY),_______,_______,_______,                    _______,_______,_______,_______,_______,KC_VOLD,
                                                _______,_______,            KC_EQL ,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAME] = LAYOUT_5x6(
      _______, KC_5  , KC_1  , KC_2  , KC_3  , KC_4  ,                         _______,_______,_______,_______,_______,_______,
      _______, KC_TAB, KC_Q  , KC_W  , KC_E  , KC_R  ,                         _______,_______,_______,_______,_______,_______,
-     _______, KC_LSFT,KC_A  , KC_S  , KC_D  , KC_F  ,                         _______,_______,_______,_______,_______,_______,
+     _______, KC_LSFT,KC_A  , KC_S  , KC_D  , KC_F  ,                         _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,
      _______, KC_B  , KC_Z  , KC_X  , KC_C  , KC_V  ,                         _______,_______,_______,_______,_______,_______,
                      _______,_______,                                                         _______,_______,
                                       KC_LALT ,KC_SPC,                        _______,_______,
@@ -87,7 +87,7 @@ void oled_task_user(void) {
   oled_write_P(PSTR("Layer: "), false);
   switch (get_highest_layer(layer_state)) {
     case _QWERTY:
-      oled_write_P(PSTR("Default\n"), false);
+      oled_write_P(PSTR("QWERTY\n"), false);
       break;
     case _RAISE:
       oled_write_P(PSTR("FN\n"), false);
@@ -98,6 +98,10 @@ void oled_task_user(void) {
     case _GAME:
       oled_write_P(PSTR("GAME\n"), false);
       break;
+    case _DVORAK:
+      oled_write_P(PSTR("DVORAK\n"), false);
+      break;
+
     default:
       // Or use the write_ln shortcut over adding '\n' to the end of your string
       oled_write_ln_P(PSTR("Undefined"), false);
