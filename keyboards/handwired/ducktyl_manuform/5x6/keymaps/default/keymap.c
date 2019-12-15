dbfd4ae5687a6e558afe40a5250717000ea50058
OutstandingOof <outstandingoof@gmail.com>
2019-12-15 15:03:11 +0100

    Changed default layer back to QWERTY

--- keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/ducktyl_manuform/5x6/keymaps/default/keymap.c
@@ -4,12 +4,11 @@
 
 extern keymap_config_t keymap_config;
 
-#define _QWERTY 1
-#define _DVORAK 0
-#define _LOWER 2
-#define _RAISE 3
-#define _MOUSE 4
-#define _GAME 5
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _MOUSE 3
+#define _GAME 4
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
@@ -19,23 +18,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6(
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_LSFT,
+     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE ,KC_SPC ,                         KC_ENT, LOWER,
-                                      KC_LGUI,KC_ESC,                         KC_BSPC,KC_LGUI,
-                                      KC_LALT,KC_GRV,                        KC_MPLY,KC_RALT
-  ), 
-
-  [_DVORAK] = LAYOUT_5x6(
-     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
-     KC_TAB ,KC_SLSH, KC_COMM, KC_DOT, KC_P , KC_Z  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_BSLASH,
-     KC_LSFT, KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , KC_T  , KC_N  , KC_S  ,KC_LSFT,
-     KC_LCTL,KC_NUBS, KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Y  ,KC_RCTL,
-                     KC_INS ,KC_CAPS,                                                        KC_RBRC,KC_EQL ,
-                                     RAISE  ,KC_SPC ,                        KC_ENT, LOWER,
-                                     KC_LGUI,KC_ESC ,                        KC_BSPC,KC_LGUI,
-                                     KC_LALT,KC_RALT,                        KC_MPLY,KC_DEL
+                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
+                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
+                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -54,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,TO(_GAME),KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        DF(_DVORAK),KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,DF(_QWERTY),_______,_______,_______,                    _______,TO(_MOUSE),_______,_______,_______,KC_VOLD,
+       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
+       _______,_______,_______,_______,_______,_______,                    _______,TO(_MOUSE),_______,_______,_______,KC_VOLD,
                                                _______,_______,            KC_EQL ,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
@@ -120,9 +108,6 @@ void oled_task_user(void) {
       break;
     case _GAME:
       oled_write_P(PSTR("GAME\n"), false);
-      break;
-    case _DVORAK:
-      oled_write_P(PSTR("DVORAK\n"), false);
       break;
 
     default:
